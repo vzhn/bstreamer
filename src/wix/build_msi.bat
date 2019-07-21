0b3b5995fcf466eb4b29beba31fe8b1af94f50f4
vzhilin <zhilin.vladimir.nic@gmail.com>
2019-07-21 17:57:17 +0300

    wix scripts

--- src/wix/build_msi.bat
+++ src/wix/build_msi.bat
@@ -1,3 +1,3 @@
 heat dir target\appassembler\repo -dr RepoDir -cg RepoGroup -gg -sfrag -template fragment -out repo.wxs
-candle mediaserver.wxs repo.wxs
+candle src\wix\mediaserver.wxs repo.wxs
 light -b target\appassembler\repo repo.wixobj mediaserver.wixobj -o mediaserver.msi
