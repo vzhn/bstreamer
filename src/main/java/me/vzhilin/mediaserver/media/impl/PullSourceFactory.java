36200041c7e60cbc50a5e91f87cc0d78684a6fc7
vzhilin <zhilin.vladimir.nic@gmail.com>
2019-09-08 11:12:11 +0300

    unused code

--- src/main/java/me/vzhilin/mediaserver/media/impl/PullSourceFactory.java
+++
@@ -1,9 +0,0 @@
-package me.vzhilin.mediaserver.media.impl;
-
-import me.vzhilin.mediaserver.conf.PropertyMap;
-import me.vzhilin.mediaserver.media.PullSource;
-import me.vzhilin.mediaserver.server.ServerContext;
-
-public interface PullSourceFactory {
-    PullSource newSource(ServerContext context, PropertyMap sourceConfig);
-}
