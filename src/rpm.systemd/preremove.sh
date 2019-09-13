ed89d2f1571c7da7f34004e1ccece0c6e2c154e4
vzhilin <zhilin.vladimir.nic@gmail.com>
2019-09-13 19:37:15 +0300

    removing extra code

--- src/rpm.systemd/preremove.sh
+++
@@ -1,2 +0,0 @@
-systemctl stop mediaserver >/dev/null
-systemctl disable mediaserver >/dev/null
--- src/rpm/preinstall.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-getent group %{mediaserver_group} >/dev/null || groupadd -r %{mediaserver_group}
-getent passwd %{mediaserver_user} >/dev/null || /usr/sbin/useradd --comment "Mediaserver" --shell /sbin/nologin -M -r -g %{mediaserver_group} --home %{mediaserver_home} %{mediaserver_user}
