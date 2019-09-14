663ad4d83bb5066abe774debf00844bc09aaa9ef
vzhilin <zhilin.vladimir.nic@gmail.com>
2019-09-14 09:14:43 +0300

    renaming

--- src/wix/build_msi.bat
+++ src/wix/build_msi.bat
@@ -1,3 +1,3 @@
 heat dir target\appassembler\repo -dr RepoDir -cg RepoGroup -gg -sfrag -template fragment -out repo.wxs
-candle src\wix\mediaserver.wxs repo.wxs
-light -b target\appassembler\repo repo.wixobj mediaserver.wixobj -o mediaserver.msi
+candle src\wix\bstreamer.wxs repo.wxs
+light -b target\appassembler\repo repo.wixobj bstreamer.wixobj -o bstreamer.msi
--- src/wix/uninstall_service.bat
+++
@@ -1,3 +0,0 @@
-set SERVICE_NAME=Mediaserver
-prunsrv.exe //SS//%SERVICE_NAME%
-prunsrv.exe //DS//%SERVICE_NAME%
