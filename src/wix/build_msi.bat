c7fd512e70177a0d772e3772cf6c2346f82e9b84
vzhilin <zhilin.vladimir.nic@gmail.com>
2019-09-15 10:32:12 +0300

    fix msi: include conf folder

--- src/wix/build_msi.bat
+++ src/wix/build_msi.bat
@@ -1,4 +1,11 @@
-heat dir target\appassembler\repo -dr RepoDir -cg RepoGroup -gg -sfrag -template fragment -out repo.wxs
-heat dir target\appassembler\bin -dr BinDir -cg BinGroup -gg -sfrag -template fragment -out bin.wxs
-candle src\wix\bstreamer.wxs repo.wxs bin.wxs
-light -b target\appassembler\bin -b target\appassembler\repo repo.wixobj bin.wixobj bstreamer.wixobj -o bstreamer.msi
+set REPODIR=target\appassembler\repo
+set BINDIR=target\appassembler\bin
+set CONFDIR=src\conf
+
+heat dir %REPODIR% -dr RepoDir -cg RepoGroup -gg -sfrag -template fragment -out repo.wxs
+heat dir %BINDIR% -dr BinDir -cg BinGroup -gg -sfrag -template fragment -out bin.wxs
+heat dir %CONFDIR% -dr ConfDir -cg ConfGroup -gg -sfrag -template fragment -out conf.wxs
+
+candle src\wix\bstreamer.wxs repo.wxs bin.wxs conf.wxs
+light -b %BINDIR% -b %REPODIR% -b %CONFDIR% ^
+  conf.wixobj repo.wixobj bin.wixobj bstreamer.wixobj -o bstreamer.msi
