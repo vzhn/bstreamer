12b6df3ded3a77b82d716f1aa4934d8b496db61b
vzhilin <zhilin.vladimir.nic@gmail.com>
2019-09-15 08:24:21 +0300

    wix: fix paths

--- src/wix/build_msi.bat
+++ src/wix/build_msi.bat
@@ -1,4 +1,4 @@
 heat dir target\appassembler\repo -dr RepoDir -cg RepoGroup -gg -sfrag -template fragment -out repo.wxs
-heat dir target\appassembler\bin -dr RepoDir -cg RepoGroup -gg -sfrag -template fragment -out bin.wxs
+heat dir target\appassembler\bin -dr BinDir -cg BinGroup -gg -sfrag -template fragment -out bin.wxs
 candle src\wix\bstreamer.wxs repo.wxs bin.wxs
 light -b target\appassembler\repo repo.wixobj bin.wixobj bstreamer.wixobj -o bstreamer.msi
