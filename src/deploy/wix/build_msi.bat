96296499e96c508b52d56fefedf4243afa0f9564
vzhilin <zhilin.vladimir.nic@gmail.com>
2019-09-20 22:56:25 +0300

    move packaging scripts to "deploy" folder

--- src/deploy/wix/build_msi.bat
+++ src/deploy/wix/build_msi.bat
@@ -1,7 +1,7 @@
 set REPODIR=target\appassembler\repo
 set BINDIR=target\appassembler\bin
-set CONFDIR=src\conf
-set VIDEODIR=src\video
+set CONFDIR=src\deploy\conf
+set VIDEODIR=src\deploy\video
 
 IF NOT "%VIDEO_LOCATION%"=="" (set VIDEODIR=%VIDEO_LOCATION%)
 
@@ -10,6 +10,6 @@ heat dir %BINDIR% -dr BinDir -cg BinGroup -gg -sfrag -template fragment -out bin
 heat dir %CONFDIR% -dr ConfDir -cg ConfGroup -gg -sfrag -template fragment -out conf.wxs
 heat dir %VIDEODIR% -dr VideoDir -cg VideoGroup -gg -sfrag -template fragment -out video.wxs
 
-candle src\wix\bstreamer.wxs repo.wxs bin.wxs conf.wxs video.wxs
+candle src\deploy\wix\bstreamer.wxs repo.wxs bin.wxs conf.wxs video.wxs
 light -b %BINDIR% -b %REPODIR% -b %CONFDIR% -b %VIDEODIR% ^
   conf.wixobj repo.wixobj bin.wixobj video.wixobj bstreamer.wixobj -o bstreamer.msi
