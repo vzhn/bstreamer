011bb8679b4b08ce4c01cc5c42a8a636acf31c67
Vladimir Zhilin <vzhilin.me@gmail.com>
2025-12-27 17:08:50 +0100

    use Netty 4.1.112.Final

--- src/main/java/me/vzhilin/bstreamer/server/RtspServerInitializer.java
+++ src/main/java/me/vzhilin/bstreamer/server/RtspServerInitializer.java
@@ -6,15 +6,17 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpRequestDecoder;
 import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.codec.rtsp.RtspDecoder;
+import io.netty.handler.codec.rtsp.RtspEncoder;
 
 public class RtspServerInitializer extends ChannelInitializer<SocketChannel> {
     @Override
     public void initChannel(SocketChannel channel) {
         ChannelPipeline pipeline = channel.pipeline();
         pipeline.addLast(new InterleavedFrameEncoder());
-        pipeline.addLast("http_request", new HttpRequestDecoder());
+        pipeline.addLast("http_request", new RtspDecoder());
         pipeline.addLast("http_aggregator", new HttpObjectAggregator(1024));
-        pipeline.addLast("http_response", new HttpResponseEncoder());
+        pipeline.addLast("http_response", new RtspEncoder());
         pipeline.addLast(new RtspServerHandler());
     }
 }
