8a212953d4c8dc246380052c55ad50830d77469e
Vladimir Zhilin <vzhilin.me@gmail.com>
2025-12-27 17:00:25 +0100

    libopenh264 profile mapping

--- src/main/java/me/vzhilin/bstreamer/server/streaming/picture/H264CodecParameters.java
+++ src/main/java/me/vzhilin/bstreamer/server/streaming/picture/H264CodecParameters.java
@@ -9,7 +9,7 @@ public class H264CodecParameters {
     private int bitrate = 400000;
     private int width = 640;
     private int height = 480;
-    private AVRational timebase;
+    private final AVRational timebase;
     private int gopSize = 10;
     private int maxBFrames = 1;
     private int fps;
@@ -77,8 +77,25 @@ public class H264CodecParameters {
         c.time_base(timebase);
         c.gop_size(gopSize);
         c.max_b_frames(maxBFrames);
-        if (profile != null && !"".equals(profile)) {
-            av_opt_set(c.priv_data(), "profile", profile, 0);
+        if (profile != null && !profile.isEmpty()) {
+            if ("libopenh264".equals(c.codec().name().getString())) {
+                String libopenh264_profileId;
+                switch (profile) {
+                    case "baseline":
+                        libopenh264_profileId = "66";
+                        break;
+                    case "main":
+                        libopenh264_profileId = "77";
+                        break;
+                    case "high":
+                        libopenh264_profileId = "100";
+                        break;
+                    default: libopenh264_profileId = "66";
+                }
+                av_opt_set(c.priv_data(), "profile", libopenh264_profileId, 0);
+            } else {
+                av_opt_set(c.priv_data(), "profile", profile, 0);
+            }
         }
     }
 
